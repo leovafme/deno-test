046a630004014c8e99844c48be60d53a355300bb
Leonardo Valencia <leovafme@hotmail.com>
2021-03-30 21:02:01 -0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,10 @@
+import { serve } from 'https://deno.land/std/http/server.ts'
+
+const PORT = Number(Deno.env.get("PORT")) || 9090;
+
+const s = serve({ port: PORT })
+console.log('http://localhost:' + PORT)
+
+for await (const req of s) {
+  req.respond({ body: 'Hello World\n' })
+}
